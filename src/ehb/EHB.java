d72d248e950d78ff791163803969bd3c0948c358
CS-Labs <cseely@unm.edu>
2018-03-21 12:36:07 -0600

    state controller framework.

--- src/ehb/EHB.java
+++ src/ehb/EHB.java
@@ -1,22 +1,16 @@
 package ehb;
 
-import interfaces.ButtonColorTypes;
-import interfaces.ButtonInterface;
+import java.util.List;
+import java.util.Map;
 
 public class EHB
 {
-  /**
-   *
-   * TODO: Implement me
-   *
-   * Method called by the engine with each simulation tick.
-   * Do not place any infinite loops in this method.
-   *
-   */
-
   private Rules _rules;
   private Events _events;
   private Actions _actions;
+  private StateTypes _currentState;
+  private Map<EventTypes, StateTypes> _inScopeStateChanges;
+  private List<ActionTypes> _inScopeActions;
 
   public EHB()
   {
@@ -27,13 +21,22 @@ public class EHB
     _rules = new Rules();
     _events = new Events(brake, motion, button);
     _actions = new Actions(brake, alarm);
+    _currentState = StateTypes.MOVING_DISENGAGED; // I assume this it the correct starting state?
   }
-
-
 
 
   public void update()
   {
-
+      _inScopeStateChanges = _rules.whatEvents(_currentState); // Get in scope potential state changes.
+      for (EventTypes event : _inScopeStateChanges.keySet()) // Check if any of the events have occurred.
+      {
+        if(_events.didEventOccur(event)) // Event occurred.
+        {
+          _inScopeActions = _rules.whatActions(event, _currentState); // Get actions to perform.
+          for(ActionTypes action : _inScopeActions) _actions.execute(action); // Execute actions.
+          _currentState = _inScopeStateChanges.get(event); // Perform state transition.
+          break;
+        }
+      }
   }
 }
