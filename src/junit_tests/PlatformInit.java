4db99a36f7ba65cc6445ff54f9746a1dcdcb3444
Jeffrey McCall <jmccall2@unm.edu>
2018-03-23 15:18:08 -0600

    Moved RunTest and InitEngine to PlatformInit.

---
+++ src/junit_tests/PlatformInit.java
@@ -0,0 +1,60 @@
+package junit_tests;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.Stage;
+import simulation.engine.Engine;
+class InitEngine {
+  private AtomicBoolean _isInitialized = new AtomicBoolean();
+
+  public InitEngine() {
+    _isInitialized.set(false);
+   }
+
+  public void init() {
+    // This forces the JavaFX runtime to initialize itself - it's a hack
+    JFXPanel panel = new JFXPanel();
+    // Platform sets it up to run on the JavaFX event dispatch thread
+    Platform.runLater(() ->
+    {
+      new Engine().start(new Stage());
+      _isInitialized.set(true);
+    });
+  }
+
+  public boolean isInitialized() {
+    return _isInitialized.get();
+  }
+}
+
+// This interface only requires that one simple method be implemented
+interface BasicTest {
+  void executeTest();
+}
+
+// Forces a test to run on the JavaFX thread and provides a thread-safe
+// means of checking if it is done
+class RunTest {
+  private AtomicBoolean _isComplete = new AtomicBoolean();
+
+  public RunTest() {
+    _isComplete.set(true);
+  }
+
+  public void execute(BasicTest test) {
+    _isComplete.set(false);
+    Platform.runLater(() ->
+    {
+      test.executeTest();
+      // Test has now executed on the JFX thread successfully
+      _isComplete.set(true);
+    });
+  }
+
+  public boolean isComplete()
+  {
+    return _isComplete.get();
+  }
+}
